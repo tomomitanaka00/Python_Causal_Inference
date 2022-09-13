1706ce05c80b6c8459d8d7260d4cff0a4639f2fe
Tomomi Tanaka <tomomit@gmail.com>
2022-09-13 15:29:57 +0900

    Create Fixed_effects_regressions.py
    Fixed effects regressions

---
+++ Fixed_effects_regressions.py
@@ -0,0 +1,24 @@
+# Example 14.2 in Woodridge "Introductory Econometrics: A Modern Approach"
+
+import wooldridge as woo
+import pandas as pd
+import linearmodels as plm
+
+# changes in return to education 
+wagepan = woo.dataWoo('wagepan')
+
+# index variables 'nr', 'year': for individuals and years
+wagepan = wagepan.set_index(['nr', 'year'], drop=False)
+
+# FE model estimation:
+reg = plm.PanelOLS.from_formula(
+    formula='lwage ~ married + union + C(year)*educ + EntityEffects',
+    data=wagepan, drop_absorbed=True)
+results = reg.fit()
+
+# print regression table:
+table = pd.DataFrame({'b': round(results.params, 4),
+                      'se': round(results.std_errors, 4),
+                      't': round(results.tstats, 4),
+                      'pval': round(results.pvalues, 4)})
+print(f'table: \n{table}\n')
